bbb508acebcd4266b6ff024178d0476c722c1a33
Ilya Slepov <brineone@gmail.com>
2021-03-16 23:06:24 +0400

    Update first_task.py

--- second_module/first_task.py
+++ second_module/first_task.py
@@ -7,7 +7,7 @@ def willson(number):
         print(f"{number} не простое")
 
 def fermat(number):
-    for i in range(1, number * number + 1):
+    for i in range(2, number * number + 1):
         if number % i != 0:
             if (pow(i, number - 1) - 1) % number == 0:
                 print(f"{number} простое")
@@ -16,7 +16,7 @@ def fermat(number):
             break
 
 print("Критерий Вильсона: ")
-willson(5)
+willson(7)
 willson(4)
 
 print("Тест на основе малой теоремы Ферма: ")
